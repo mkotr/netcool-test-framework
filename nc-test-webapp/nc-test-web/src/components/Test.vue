fc11370c82311a4d4b27b52e2186dfc230aacc94
Morokot Ros <mkot1994@gmail.com>
2019-09-29 19:01:38 +1000

    Added vue frontend

---
+++ nc-test-webapp/nc-test-web/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello! {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style></style>
---
+++ nc-test-webapp/nc-test-web/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Test msg="Morokot" />
+  </div>
+</template>
+
+<script>
+import Test from '@/components/Test.vue'
+export default {
+  name: 'about',
+  components: {
+    Test
+  }
+}
+</script>
